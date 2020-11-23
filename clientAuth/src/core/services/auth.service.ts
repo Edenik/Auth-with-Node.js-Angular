6c45a3ed2c78e886aa7db54b67627d23d8e76858
Edenik <Edenik5@gmail.com>
2020-11-23 19:22:08 +0200

    Auth Services

--- clientAuth/src/core/services/auth.service.ts
+++ clientAuth/src/core/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
 import {User } from '../models/user'
@@ -8,7 +9,7 @@ import {User } from '../models/user'
 })
 export class AuthService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient, private router:Router) { }
 
   login(user:User) :Observable<User> {
     return this.http.post<User>(`${environment.apiEvents}/auth/login`, user);
@@ -16,5 +17,18 @@ export class AuthService {
 
   register(user:User) :Observable<any> {
     return this.http.post<any>(`${environment.apiEvents}/auth/register`, user);
+  }
+
+  logOut(){
+    localStorage.removeItem('token');
+    this.router.navigate(['/events']);
+  }
+
+  getToken(){
+    return localStorage.getItem('token');
+  }
+
+  isLoggedIn(){
+    return !!localStorage.getItem('token');
   }
 }
